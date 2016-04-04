c2c891730d98c7f7dd844bd345acada42c545f0a
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2016-04-04 20:44:39 +0200

    Refactor

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -5,31 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
-use View;
-
 use DOMPDF;
 
 use App\Http\Requests;
+use View;
 
 class PDFController extends Controller
 {
     public function invoiceHtml()
     {
-        $descriptions = [ "description1", "description2", "description3", "description4"];
-        $subscriptions = [ 45, 46, 145, 458];
-        $data = [
-            'vendor' => 'PROVA',
-            'user' => 'Sergi',
-            'email' => 'sergiturbadenas@gmail.com',
-            'name' => 'Sergi Tur',
-            'product' => 'Producte 1',
-            'descriptions' => $descriptions,
-            'subscriptions' => $subscriptions,
-            'hasDiscount' => true,
-            'discount' => "20%",
-            'tax_percent' => "23%",
-            'tax' => "456"
-        ];
+        $data = $this->getData();
         return view('receipt',$data);
     }
 
@@ -43,13 +28,8 @@ class PDFController extends Controller
         }
         $dompdf = new Dompdf();
 
-        $data = [
-            'vendor' => 'PROVA',
-            'user' => 'Sergi'
-        ];
-
 //        $dompdf->load_html("<h1>Hola</h1>");
-        $dompdf->load_html($this->view($data)->render());
+        $dompdf->load_html($this->view($this->getData())->render());
 
         $dompdf->render();
         return $this->download($dompdf->output());
@@ -80,6 +60,29 @@ class PDFController extends Controller
      */
     public function view(array $data)
     {
-        return $this->app->make('view')->make('receipt', $data));
+        return View::make('receipt', $data);
+    }
+
+    /**
+     * @return array
+     */
+    private function getData()
+    {
+        $descriptions = ["description1", "description2", "description3", "description4"];
+        $subscriptions = [45, 46, 145, 458];
+        $data = [
+            'vendor' => 'PROVA',
+            'user' => 'Sergi',
+            'email' => 'sergiturbadenas@gmail.com',
+            'name' => 'Sergi Tur',
+            'product' => 'Producte 1',
+            'descriptions' => $descriptions,
+            'subscriptions' => $subscriptions,
+            'hasDiscount' => true,
+            'discount' => "20%",
+            'tax_percent' => "23%",
+            'tax' => "456"
+        ];
+        return $data;
     }
 }
